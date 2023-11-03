e184479a09038d0bc64c31ec07ebeb63646c360b
Cbcasper <casper.desmet@student.uantwerpen.be>
2023-11-03 13:20:57 +0100

    standardised datasets and fieldsets

--- word-embeddings-explorer/src/lib/Collapse.svelte
+++
@@ -1,7 +0,0 @@
-<script>
-    import FaBars from 'svelte-icons/fa/FaBars.svelte'
-</script>
-
-<div class="w-10 h-10 text-sky-800">
-    <FaBars/>
-</div>
--- word-embeddings/src/device.py
+++
@@ -1,7 +0,0 @@
-import torch
-
-device = torch.device("cpu")
-if torch.backends.mps.is_available():
-    device = torch.device("mps")
-if torch.cuda.is_available():
-    device = torch.device("cuda:0")
